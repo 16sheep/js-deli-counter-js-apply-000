b7e99450478f0abf6cbc6eedd0922ec447482e68
marjutubli@gmail.com <marjutubli@gmail.com>
2018-04-19 05:57:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const takeANumber = (katzDeliLine, newPerson) => {
   katzDeliLine.push(newPerson);
   return 'Welcome, '+ newPerson + '. You are number ' + katzDeliLine.length + ' in line.';
 };
+
+const nowServing (katzDeliLine); // "Currently serving Ada."
